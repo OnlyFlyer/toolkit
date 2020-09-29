4ca98138e2bb833b5398af05bdeecaa58ad3c353
wutao <m15776849661@163.com>
2020-09-29 11:57:49 +0800

    feat: add request method

---
+++ test_request.tsx
@@ -0,0 +1,25 @@
+import axios, { AxiosRequestConfig } from 'axios'
+
+// export interface RequestProps {
+//   url?: string
+//   path?: string
+//   method?: string
+//   params?: any
+//   headers?: any
+//   config?: any
+// }
+
+export const request: any = (props: AxiosRequestConfig) => {
+  try {
+    console.log('request---1')
+    return axios.create(props)
+  } catch (err) {
+    console.log('err:', err)
+    return null
+  }
+}
+
+request.defaultProps = {
+  url: process.env.GATEWAY,
+  path: '/gw/api/'
+} as Partial<AxiosRequestConfig>
