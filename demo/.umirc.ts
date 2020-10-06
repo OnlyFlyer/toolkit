cdb5a7b7afef3f374805650f941a409ce6d06be5
wutao <m15776849661@163.com>
2020-10-06 12:39:38 +0800

    feat: add antd pro aside

--- demo/.umirc.ts
+++ demo/.umirc.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'umi';
+import { defineConfig } from 'umi'
 
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
+  layout: {},
   routes: [
     { path: '/', component: '@/pages/index' },
   ],
