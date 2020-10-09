3dbbb01133cf7681b1d612617afcb6700c8b50d0
wutao <m15776849661@163.com>
2020-10-09 15:31:04 +0800

    feat: log

--- demo/src/pages/index.tsx
+++ demo/src/pages/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from './index.less';
 
 export default () => {
+  console.log('process:', process)
+  console.log('env:', process.env)
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
