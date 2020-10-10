ac52de07c8fe66b1c7d6eb60b6123467085930e2
wutao <m15776849661@163.com>
2020-10-10 09:37:43 +0800

    feat: request

---
+++ packages/request/src/helper.ts
@@ -0,0 +1,7 @@
+export const zxc = 'zxc'
+
+export const defaultHeaders = {
+  // 'Content-type': 'application/x-www-form-urlencoded',
+  'Content-Type': 'multipart/form-data',
+  appKey: process.env.APP_KEY
+}
--- packages/request/src/index.ts
+++ packages/request/src/index.ts
@@ -1,15 +1,43 @@
-import axios, { AxiosRequestConfig } from 'axios'
+import axios, { AxiosRequestConfig, AxiosError, AxiosResponse } from 'axios'
 
-// export interface RequestProps {
-//   url?: string
-//   path?: string
-//   method?: string
-//   params?: any
-//   headers?: any
-//   config?: any
-// }
+interface RequestProps {
+  api: string
+  params?: any
+  headers?: any
+  config?: AxiosRequestConfig
+}
 
-export const request: any = (props: AxiosRequestConfig) => {
+
+const customAxios = axios.create({
+  timeout: 60000,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+})
+
+customAxios.interceptors.response.use(
+  (val: AxiosResponse) => {
+    const { data } = val
+    // 图片上传接口返回的是result
+    const { success, response, errorResponse = {}, result } = data
+    if (success) {
+      return response || result
+    } else {
+      if (+errorResponse.code === 9100) {
+        const { APP_KEY, LOGIN } = process.env
+        const { href } = window.location
+        window.location.href = `${LOGIN}?appKey=${APP_KEY}&goto_page=${encodeURIComponent(href)}`
+        return
+      }
+      return Promise.reject(errorResponse)
+    }
+  },
+  (err: AxiosError) => Promise.reject(err)
+)
+
+
+export const request: any = (props: AxiosRequestConfig & RequestProps) => {
   try {
     console.log('request---1')
     return axios.create(props)
@@ -22,4 +50,5 @@ export const request: any = (props: AxiosRequestConfig) => {
 request.defaultProps = {
   url: process.env.GATEWAY,
   path: '/gw/api/'
-} as Partial<AxiosRequestConfig>
+}
+// as Partial<AxiosRequestConfig>
