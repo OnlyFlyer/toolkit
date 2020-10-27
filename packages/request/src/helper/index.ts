7a01df6ecbbadcb3de6ac69159b0770217b82ad7
wutao <m15776849661@163.com>
2020-10-27 15:09:31 +0800

    feat: request over

--- packages/request/src/helper/index.ts
+++ packages/request/src/helper/index.ts
@@ -1,4 +1,6 @@
 import { v4 as uuidv4 } from 'uuid'
+import Fingerprint2 from 'fingerprintjs2sync'
+import UAParser from 'ua-parser-js'
 
 export const defaultGW: string = 'https://gateway.songxiaocai.com'
 
@@ -14,9 +16,9 @@ export const defaultHeaders = {
 export const defaultQuery = {
   appKey: '86683443',
   bizCode: 'devops_login',
-  clientSysName: 'MAC OSX',
-  clientSysVersion: 'v99.99.99',
-  clientVersion: 'MAC OSX',
+  clientSysName: 'Safari_DEFAULT',
+  clientSysVersion: 'v99.99.99_DEFAULT',
+  clientVersion: 'Mac OS_DEFAULT',
   deviceUUID: Math.ceil(Math.random() * 10e10)
 }
 
@@ -25,22 +27,30 @@ export const getDefaultQuery = () => {
   try {
     // 若没有透传就不能用 `const { APP_KEY, BIZ_CODE } = process.env` 解构出来，所以粗暴点直接用
     if (process.env.APP_KEY) {
-      Object.assign(query, {
-        appKey: process.env.APP_KEY
-      })
+      Object.assign(query, { appKey: process.env.APP_KEY })
     }
     if (process.env.BIZ_CODE) {
-      Object.assign(query, {
-        bizCode: process.env.BIZ_CODE
-      })
+      Object.assign(query, { bizCode: process.env.BIZ_CODE })
     }
   } catch (err) {}
   try {
+    const deviceUUID = new Fingerprint2().getSync().fprint
+    Object.assign(query, { deviceUUID })
+  } catch (err) {
     if (uuidv4()) {
-      Object.assign(query, {
-        deviceUUID: uuidv4()
-      })
+      Object.assign(query, { deviceUUID: uuidv4() })
     }
+  }
+  try {
+    const parser = new UAParser()
+    const os = parser.getOS()
+    const { name, version } = parser.getBrowser()
+
+    Object.assign(query, {
+      clientSysName: name,
+      clientSysVersion: version,
+      clientVersion: os.name
+    })
   } catch (err) {}
   return query
 }
