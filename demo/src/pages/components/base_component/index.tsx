5a3c5ffe526fb5258f469ad765ff6f86b4e04094
wutao <m15776849661@163.com>
2020-10-28 10:44:45 +0800

    feat: 防重复组件 over

--- demo/src/pages/components/base_component/index.tsx
+++ demo/src/pages/components/base_component/index.tsx
@@ -3,9 +3,47 @@ import React from 'react'
 import { AvoidRepeatBaseComponent } from '../../../modules/base_component/src/index'
 
 export class AvoidRepeat extends AvoidRepeatBaseComponent {
-  justOnce = true
+  justOnce = false
+  handleRepeat = async () => {
+    try {
+      const { post } = this
+      const res = await post({ api: 'bb' })
+      console.log('res:', res)
+    } catch (err) {}
+  }
   render () {
-    console.log('外部 this:', this)
-    return <div>AvoidRepeat Component</div>
+    return <div>
+      <div onClick={this.handleRepeat}>AvoidRepeat Component</div>
+      <Btn1 />
+      <Btn2 />
+    </div>
+  }
+}
+
+export class Btn1 extends AvoidRepeatBaseComponent {
+  justOnce = false
+  handleRepeat = async () => {
+    try {
+      const { post } = this
+      const res = await post({ api: 'btn1' })
+      console.log('btn1:', res)
+    } catch (err) {}
+  }
+  render () {
+    return <div onClick={this.handleRepeat}>Btn1</div>
+  }
+}
+
+export class Btn2 extends AvoidRepeatBaseComponent {
+  justOnce = true
+  handleRepeat = async () => {
+    try {
+      const { post } = this
+      const res = await post({ api: 'btn2' })
+      console.log('btn2:', res)
+    } catch (err) {}
+  }
+  render () {
+    return <div onClick={this.handleRepeat}>Btn2</div>
   }
 }
--- packages/base_component/src/index.ts
+++ packages/base_component/src/index.ts
@@ -2,25 +2,35 @@ import { Component, useState, useCallback, useEffect, useRef } from 'react'
 
 import { request, RequestProps } from '../../request/src/index'
 
-// var request: any = () => {}
-
 const TOEKN_URL = 'sxc.acp.repeatToken.get'
 
-// export interface AvoidRepeatCompProps {}
+export interface AvoidRepeatCompState {
+  avoidRepeatToken: string | undefined
+  pool: any
+  fetchTokenNums: number
+}
 
 
 export class AvoidRepeatBaseComponent extends Component {
-  state = {
+  state: AvoidRepeatCompState = {
     avoidRepeatToken: undefined,
-    pool: {}
+    pool: {},
+    fetchTokenNums: 0
   }
 
-  justOnce = false
+  public justOnce = false
 
   async initToken() {
+    const { fetchTokenNums } = this.state
+    const { justOnce } = this
+    // 如果只允许发起一次请求且已经请求过一次了，直接返回，不再获取 token
+    if (!!justOnce && fetchTokenNums >= 1) return
     try {
       const { token } = await request({ api: TOEKN_URL })
-      this.setState({ avoidRepeatToken: token })
+      this.setState({
+        avoidRepeatToken: token,
+        fetchTokenNums: fetchTokenNums + 1
+      })
     } catch(err) {}
   }
 
@@ -36,34 +46,35 @@ export class AvoidRepeatBaseComponent extends Component {
     this.initToken()
   }
 
-  post = ({ api, headers = {}, ...rest }: any) => new Promise((resolve, reject) => {
+  post = ({
+    api,
+    headers = {},
+    ...rest
+  }: RequestProps) => new Promise((resolve, reject) => {
     const { avoidRepeatToken, pool } = this.state
     if (!api) return
     if (!pool[api]) {
-      this.setState({ pool: {...pool, [api]: { loading: true } } })
+      this.setState({
+        pool: { ...pool, [api]: { loading: true } }
+      })
     } else {
       if (pool[api].loading) return
     }
     const p = { api, ...rest }
     if (avoidRepeatToken) {
       Object.assign(p, {
-        headers: {
-          ...(headers || {}),
-          'rp-check-tk': avoidRepeatToken
-        }
+        headers: { ...(headers || {}), 'rp-check-tk': avoidRepeatToken }
       })
     }
     request(p)
-      .then((e) => {
-        resolve(e)
-      })
-      .catch((err) => {
-        reject(err)
-      })
+      .then((data: any) => resolve(data))
+      .catch((err: any) => reject(err))
       .finally(() => {
         this.setState({
-          ...pool,
-          [api]: { loading: false },
+          pool: {
+            ...pool,
+            [api]: { loading: false },
+          },
           avoidRepeatToken: undefined
         }, () => {
           this.initToken()
@@ -84,9 +95,12 @@ export const useAvoidRepeatHook = (p: AvoidRepeatHookProps) => {
   const { justOnce = false } = p
 
   const initToken = useCallback(async () => {
+    // 如果只允许发起一次请求且已经请求过一次了，直接返回，不再获取 token
+    if (!!justOnce && requestNumsRef.current >= 1) return
     try {
       const res = await request({ api: TOEKN_URL })
       tokenRef.current = res.token
+      requestNumsRef.current = requestNumsRef.current + 1
     } catch (err) {}
   }, [])
 
@@ -98,31 +112,28 @@ export const useAvoidRepeatHook = (p: AvoidRepeatHookProps) => {
     initToken()
   }, [initToken])
 
-  const post = async (params: RequestProps) =>
-  new Promise((resolve, reject) => {
-    if (!params.api) return
-    // 如果只允许发起一次请求且已经请求过一次了，直接返回
-    if (!!justOnce && requestNumsRef.current >= 1) return
-    if (!pool[params.api]) {
-      setPool({ ...pool, [params.api]: { loading: true } })
+  const post = async ({
+    api,
+    headers = {},
+    ...rest
+  }: RequestProps) => new Promise((resolve, reject) => {
+    if (!api) return
+    if (!pool[api]) {
+      setPool({ ...pool, [api]: { loading: true } })
     } else {
-      if (pool[params.api].loading) return
+      if (pool[api].loading) return
     }
-    const p = { ...params }
+    const p = { api, ...rest }
     if (tokenRef.current) {
       Object.assign(p, {
-        headers: {
-          ...(params.headers || {}),
-          'rp-check-tk': tokenRef.current
-        }
+        headers: { ...(headers || {}), 'rp-check-tk': tokenRef.current }
       })
     }
     request(p)
       .then((data: any) => resolve(data))
       .catch((err: any) => reject(err))
       .finally(() => {
-        requestNumsRef.current = requestNumsRef.current + 1
-        setPool({ ...pool, [params.api]: { loading: false } })
+        setPool({ ...pool, [api]: { loading: false } })
         tokenRef.current = undefined
         if (!!justOnce) return
         initToken()
